9ea91e5a63744a8387b01ea9f0965e7f3ee5b281
Yu Nakanishi <i@youcune.com>
2012-11-26 22:56:00 +0900

    初版

---
+++ AccuratePosition.js
@@ -0,0 +1,70 @@
+// AccuratePosition
+// 時間の許す限り正確な位置情報を取得する
+// http://pear.jp/
+ 
+function getAccuratePosition(successCallback, errorCallback, option){
+    // 位置情報に対応していなければ終了
+    if(!navigator.geolocation){
+        var error     = new Object();
+        error.code    = 9;
+        error.message = 'Geolocation APIが利用できません。';
+        errorCallback(error);
+        return;
+    }
+ 
+    // 変数の初期化
+    var watch_id = undefined;
+    var timer_id = undefined;
+    var position = undefined;
+    var limit    = option && option.limit   ? option.limit   : 100;
+    var timeout  = option && option.timeout ? option.timeout : 0;
+ 
+    // タイムアウトをセット
+    if(timeout > 0){
+        timer_id = setTimeout(
+            function(){
+                // 位置情報の取得を中止する
+                if(watch_id){
+                    navigator.geolocation.clearWatch(watch_id);
+                    watch_id = undefined;
+                }
+ 
+                // 位置情報が取得できていればsuccessCallbackに送る
+                if(position){
+                    successCallback(position);
+                }
+                else{
+                    var error     = new Object();
+                    error.code    = 9;
+                    error.message = '位置情報の取得でタイムアウトしました。';
+                    errorCallback(error);
+                    return;
+                }
+            }, 
+            timeout
+        );
+    }
+ 
+    // 取得を実行
+    watch_id = navigator.geolocation.watchPosition(
+        function(p){
+            // 取得のたびに更新する
+            position = p;
+ 
+            // 求める精度に達すればsuccessCallbackに送る
+            if(position.coords.accuracy < limit){
+                // タイムアウト監視を止める
+                if(timer_id){
+                    clearTimeout(timer_id);
+                    timer_id = undefined;
+                }
+                navigator.geolocation.clearWatch(watch_id);
+                successCallback(position);
+            }
+        }, 
+        function(e){
+            errorCallback(e);
+        }, 
+        {enableHighAccuracy:true, maximumAge:0}
+    );
+}
